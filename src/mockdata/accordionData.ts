fbbf72ebeb6b6ba11cd5571288ac4787df60962c
qgatssdev <chivoomodu@gmail.com>
2023-08-25 23:57:50 +0100

    mock data

---
+++ src/mockdata/accordionData.ts
@@ -0,0 +1,20 @@
+export type AccordionData = {
+  id: number;
+  title: string;
+  content: string;
+};
+
+export const accordionData: AccordionData[] = [
+  {
+    id: 1,
+    title: 'We connect our customers with the best?',
+    content:
+      'At Boldo, our mission is to provide unparalleled value and exceptional solutions to our customers. We take pride in our commitment to connecting our clients with the very best that the industry has to offer',
+  },
+  {
+    id: 2,
+    title: 'Android research & development rockstar? ',
+    content:
+      'At Boldo, our Android Research & Development (R&D) team stands out as the true rockstars of innovation and creativity. Heres why we believe our Android R&D team is nothing short of rockstars:',
+  },
+];
---
+++ src/mockdata/blogposts.ts
@@ -0,0 +1,63 @@
+export type BlogPostsProps = {
+  id: number;
+  blogImage: string;
+  category: string;
+  title: string;
+  avatar: string;
+  author: string;
+};
+
+export const blogPosts: BlogPostsProps[] = [
+  {
+    id: 1,
+    blogImage: require('@assets/blogposts/officeman.png'),
+    category: 'November 22 2021',
+    title: 'Pitch termsheet backing validation focus release.',
+    avatar: require('@assets/blogposts/chandler bling.png'),
+    author: 'Chandler Bling',
+  },
+  {
+    id: 2,
+    blogImage: require('@assets/blogposts/woman on phone.png'),
+    category: 'November 22 2021',
+    title:
+      'Seed round direct mailing non-disclosure agreement graphical user interface rockstar.',
+    avatar: require('@assets/blogposts/rachel green.png'),
+    author: 'Rachel Green',
+  },
+  {
+    id: 3,
+    blogImage: require('@assets/blogposts/folder man.png'),
+    category: 'November 22 2021',
+    title:
+      'Beta prototype sales iPad gen-z marketing network effects value proposition',
+    avatar: require('@assets/blogposts/monica geller.png'),
+    author: 'Monica Geller',
+  },
+  {
+    id: 4,
+    blogImage: require('@assets/blogposts/man on phone.png'),
+    category: 'November 22 2021',
+    title: 'Pitch termsheet backing validation focus release.',
+    avatar: require('@assets/blogposts/chandler bling.png'),
+    author: 'Chandler Bling',
+  },
+  {
+    id: 5,
+    blogImage: require('@assets/blogposts/office.png'),
+    category: 'November 22 2021',
+    title:
+      'Seed round direct mailing non-disclosure agreement graphical user interface rockstar.',
+    avatar: require('@assets/blogposts/chandler bling.png'),
+    author: 'Rachel Green',
+  },
+  {
+    id: 6,
+    blogImage: require('@assets/blogposts/agreement.png'),
+    category: 'November 22 2021',
+    title:
+      'Beta prototype sales iPad gen-z marketing network effects value proposition',
+    avatar: require('@assets/blogposts/monica geller.png'),
+    author: 'Monica Geller',
+  },
+];
