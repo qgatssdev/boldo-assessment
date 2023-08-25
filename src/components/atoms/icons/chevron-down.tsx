7f369097248f8618b42b5abfa3eb18722380b3ff
qgatssdev <chivoomodu@gmail.com>
2023-08-25 23:58:30 +0100

    new icons

---
+++ src/components/atoms/icons/chevron-down.tsx
@@ -0,0 +1,20 @@
+export const ChevronDown = () => {
+  return (
+    <svg
+      width='28'
+      height='28'
+      viewBox='0 0 28 28'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <circle cx='14' cy='14' r='14' fill='#0A2640' />
+      <path
+        d='M8 12L14 18L20 12'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
---
+++ src/components/atoms/icons/chevron-up.tsx
@@ -0,0 +1,21 @@
+export const ChevronUp = () => {
+  return (
+    <svg
+      width='28'
+      height='28'
+      viewBox='0 0 28 28'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <circle cx='14' cy='14' r='14' fill='#0A2640' />
+      <path
+        d='M20.1101 14.999L13.9989 9.11223L8.11221 15.2234'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
